ae9c9ded880181c5479c33d484a0173fad8ba025
Muhammad Rezky <mrezkysulihin@gmail.com>
2023-06-19 01:11:30 +0700

    Initial Commit

---
+++ FunQuranGame/FunQuranGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunQuranGameApp.swift
+//  FunQuranGame
+//
+//  Created by Muhammad Rezky on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FunQuranGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
